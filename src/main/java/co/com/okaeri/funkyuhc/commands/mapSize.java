af010c893b44464f42299761f24bd5577c5dbfa6
funkyo <enmafunkyo@gmail.com>
2022-11-08 17:19:19 -0500

    Se agregan los comandos para cambio del tamaño del mapa

---
+++ src/main/java/co/com/okaeri/funkyuhc/commands/mapSize.java
@@ -0,0 +1,58 @@
+package co.com.okaeri.funkyuhc.commands;
+
+import co.com.okaeri.funkyuhc.FunkyUHC;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class mapSize implements CommandExecutor{
+
+    private FunkyUHC plugin;
+
+    public mapSize(FunkyUHC main){
+        this.plugin = main;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(!(sender instanceof Player)) {
+            if (args.length == 0) {
+                size();
+                return true;
+            } else if (args.length == 2) { // Todo: verificar que el argumento [1] sea un entero
+                switch (args[0].toLowerCase()) {
+                    case "setmax":
+                        setMax(Integer.parseInt(args[1]));
+                        return true;
+                    case "set":
+                        setSize(Integer.parseInt(args[1]));
+                        return true;
+                    default:
+                        return false;
+                }
+
+            } else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    private void setMax(int value){
+        plugin.changeMaxSize(value);
+        plugin.print("Tamaño maximo del mapa cambiado a: " + value);
+    }
+
+    public void setSize(int value){
+        plugin.changeSize(value);
+        plugin.db.updateSize(value);
+        plugin.print("Tamaño del mapa cambiado a: " + value);
+    }
+
+    private int size(){
+        plugin.print(String.valueOf(plugin.size));
+        return plugin.size;
+    }
+}
