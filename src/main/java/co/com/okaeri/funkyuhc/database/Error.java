a0d963b1ffa3d7da1236f2507240f7551df44860
funkyo <enmafunkyo@gmail.com>
2022-11-08 14:54:49 -0500

    Initial commit

---
+++ src/main/java/co/com/okaeri/funkyuhc/database/Error.java
@@ -0,0 +1,14 @@
+package co.com.okaeri.funkyuhc.database;
+
+import co.com.okaeri.funkyuhc.FunkyUHC;
+
+import java.util.logging.Level;
+
+public class Error {
+    public static void execute(FunkyUHC plugin, Exception ex){
+        plugin.getLogger().log(Level.SEVERE, "Couldn't execute MySQL statement: ", ex);
+    }
+    public static void close(FunkyUHC plugin, Exception ex){
+        plugin.getLogger().log(Level.SEVERE, "Failed to close MySQL connection: ", ex);
+    }
+}
