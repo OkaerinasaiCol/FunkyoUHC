6f073cb4d11beae652b389e31bc39b9765aea6c8
funkyo <enmafunkyo@gmail.com>
2022-11-09 14:56:45 -0500

    Ejecutador del comando teams

---
+++ src/main/java/co/com/okaeri/funkyuhc/commands/Teams.java
@@ -0,0 +1,46 @@
+package co.com.okaeri.funkyuhc.commands;
+
+import co.com.okaeri.funkyuhc.FunkyUHC;
+import org.bukkit.Color;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.sql.SQLException;
+
+
+public class Teams implements CommandExecutor {
+
+    private FunkyUHC plugin;
+    private co.com.okaeri.funkyuhc.database.Teams db;
+
+    public Teams(FunkyUHC plugin){
+        this.plugin = plugin;
+        db = new co.com.okaeri.funkyuhc.database.Teams(plugin);
+    }
+
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        // TODO: mejorar el manejo de argumentos
+        switch (args[0].toLowerCase()) {
+            case "create":
+                if (args.length == 4) {
+                    try {
+                        db.Create(args[1], args[2], args[3]);
+                        return true;
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                return false;
+            case "delete":
+                plugin.print("delete");
+                return false;
+            default:
+                return false;
+
+        }
+    }
+
+}
