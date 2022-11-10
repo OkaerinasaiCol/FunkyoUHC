8453b6dd8fbf0faa2511c22d988ebbeec9be66fe
funkyo <enmafunkyo@gmail.com>
2022-11-10 17:21:19 -0500

    Se mueve la funcion para obtener la cabeza a su propia clase dentro del paquete utils

--- src/main/java/co/com/okaeri/funkyuhc/player/DeathListener.java
+++ src/main/java/co/com/okaeri/funkyuhc/player/DeathListener.java
@@ -1,25 +1,16 @@
 package co.com.okaeri.funkyuhc.player;
 
 import co.com.okaeri.funkyuhc.FunkyUHC;
-import co.com.okaeri.funkyuhc.util.Skin;
-import com.mojang.authlib.GameProfile;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
-import org.bukkit.profile.PlayerProfile;
-import org.bukkit.profile.PlayerTextures;
-
-import java.net.URL;
-import java.util.*;
+import co.com.okaeri.funkyuhc.util.Head;
 
 public class DeathListener implements Listener{
 
+    @SuppressWarnings("FieldMayBeFinal")
     private FunkyUHC plugin;
 
     public  DeathListener(FunkyUHC plugin){
@@ -30,36 +21,15 @@ public class DeathListener implements Listener{
     public void Muertes(PlayerDeathEvent e){
         Player p = e.getEntity();
 
-        ItemStack drop = getPlayerHead(p);
+        Head h = new Head();
+
+        ItemStack drop = h.getPlayerHead(p);
         p.getWorld().dropItem(p.getLocation(), drop);
 
         plugin.print(e.getEntity() + " death " + e.getEntity().getLastDamageCause() + e.getEntity().getKiller());
         plugin.print("death id" + e.getEntity().getUniqueId());
     }
 
-    public ItemStack getPlayerHead(Player p){
-        ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
-
-        PlayerProfile profile = p.getPlayerProfile();
-        // TODO: colocar la skin desde el skin restorer si es posible
-
-        PlayerProfile p_profile = p.getPlayerProfile();
-        PlayerTextures p_textures = p_profile.getTextures();
-
-        SkullMeta meta = (SkullMeta) item.getItemMeta();
-
-        //noinspection ConstantConditions
-        meta.setOwnerProfile(p_profile);
-        meta.setDisplayName("Cabeza de " + p.getName());
-
-        List<String> lore = Arrays.asList("Cabeza de: " + p.getName(), "uuid: " + p.getUniqueId());
-
-        meta.setLore(lore);
-
-        item.setItemMeta(meta);
-
-        return item;
-    }
 
 
 }
