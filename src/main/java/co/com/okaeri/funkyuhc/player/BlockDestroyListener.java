0d86cb6896650db04d9dc80a03dfd19fdb4b6526
Funkyo <enmafunkyo@gmail.com>
2022-11-11 01:15:04 -0500

    Se agrega que al destruir una cabeza te de la cabeza con el correspondiente lore

--- src/main/java/co/com/okaeri/funkyuhc/player/BlockDestroyListener.java
+++ src/main/java/co/com/okaeri/funkyuhc/player/BlockDestroyListener.java
@@ -1,10 +1,18 @@
 package co.com.okaeri.funkyuhc.player;
 
 import co.com.okaeri.funkyuhc.FunkyUHC;
+import co.com.okaeri.funkyuhc.database.Heads;
+import co.com.okaeri.funkyuhc.util.Head;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.profile.PlayerProfile;
+
+import java.util.List;
+import java.util.UUID;
 
 public class BlockDestroyListener implements Listener {
 
@@ -17,12 +25,22 @@ public class BlockDestroyListener implements Listener {
 
     @EventHandler
     public void onBlockDestroy(BlockBreakEvent event){
+        plugin.print("Placed: " + event.getBlock().getType());
+
         if (event.getBlock().getType() == Material.PLAYER_WALL_HEAD){
             event.setDropItems(false);
 
+            Heads head = new Heads(plugin);
+            Head h = new Head();
+            List<String> p_info = head.getHead(event.getBlock());
 
+            UUID uuid = UUID.fromString(p_info.get(0));
 
-            // PlayerProfile = plugin.getServer().createPlayerProfile();
+            PlayerProfile playerProfile = plugin.getServer().createPlayerProfile(uuid, p_info.get(1));
+
+            ItemStack drop = h.getPlayerHead(playerProfile);
+
+            event.getBlock().getWorld().dropItem(event.getBlock().getLocation(), drop);
         }
     }
 
