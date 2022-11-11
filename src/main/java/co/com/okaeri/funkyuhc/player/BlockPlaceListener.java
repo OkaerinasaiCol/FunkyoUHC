4616b0cce2e03287c3b670128d67ada4366cfed0
Funkyo <enmafunkyo@gmail.com>
2022-11-11 01:15:52 -0500

    Programando dinamica de revivido (Estructura)

--- src/main/java/co/com/okaeri/funkyuhc/player/BlockPlaceListener.java
+++ src/main/java/co/com/okaeri/funkyuhc/player/BlockPlaceListener.java
@@ -1,6 +1,7 @@
 package co.com.okaeri.funkyuhc.player;
 
 import co.com.okaeri.funkyuhc.FunkyUHC;
+import co.com.okaeri.funkyuhc.database.Heads;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -9,6 +10,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class BlockPlaceListener implements Listener {
 
@@ -25,13 +30,104 @@ public class BlockPlaceListener implements Listener {
         Player player = event.getPlayer();
         ItemStack item = event.getItemInHand();
         Block block = event.getBlockPlaced();
+        plugin.print("Placed: " + item.getType());
 
         if (item.getType() == Material.PLAYER_HEAD){
-            //set metadata
-            //noinspection unused
+
             ItemMeta itemmeta = event.getItemInHand().getItemMeta();
             // TODO: guardar lore en la base de datos y borrar en caso de que se quite
             plugin.heads.setHead(item, player, block);
+
+        } else if (item.getType() == Material.FLINT_AND_STEEL || item.getType() == Material.FIRE_CHARGE) {
+
+                Block start = event.getBlock();
+
+                if (start.getRelative(0, -1, 0).getType().equals(Material.SOUL_SAND)){
+
+                    List<Block> structure = new ArrayList<>();
+                    structure.add(start.getRelative(0, -1, 0));
+
+                    if (start.getRelative(1,-1,0).getType().equals(Material.GOLD_BLOCK)){
+                        structure.add(start.getRelative(1,-1,0));
+                        rightOrient(start, structure);
+                    } else if (start.getRelative(-1, -1,0).getType().equals(Material.GOLD_BLOCK)) {
+                        structure.add(start.getRelative(-1, -1,0));
+                        leftOrient(start, structure);
+                    } else if (start.getRelative(0,-1, 1).getType().equals(Material.GOLD_BLOCK)){
+                        structure.add(start.getRelative(0,-1, 1));
+                        frontOrient(start, structure);
+                    } else if (start.getRelative(0,-1, -1).getType().equals(Material.GOLD_BLOCK)) {
+                        structure.add(start.getRelative(0,-1, -1));
+                        backOrient(start, structure);
+                    }
+                }
         }
+    }
+
+    @SuppressWarnings("ReassignedVariable")
+    private void rightOrient(Block block, List<Block> structure){
+
+        // Verificar primero si del lado opuesto ya hay fuego prendio para evitar procesos innecesarios
+
+        Block c_block = block.getRelative(2, 0, 0);
+
+        plugin.print("" + c_block.getType());
+
+        if (c_block.getType().equals(Material.SOUL_FIRE)){
+            structure.add(block.getRelative(2, 1, 0));
+
+            plugin.print("derecha SOUL_FIRE");
+
+            c_block = block.getRelative(1, 0, 0);
+
+            if (c_block.getType().equals(Material.DIAMOND_BLOCK)){
+                structure.add(c_block);
+
+                plugin.print("derecha DIAMOND_BLOCK");
+
+                c_block = block.getRelative(1,1,0);
+
+                if (c_block.getType().equals(Material.DIAMOND_BLOCK)){
+                    structure.add(c_block);
+
+                    plugin.print("derecha DIAMOND_BLOCK");
+
+                    c_block = block.getRelative(1, 2,0);
+
+                    if (c_block.getType().equals(Material.PLAYER_HEAD)){
+                        structure.add(c_block);
+
+                        plugin.print("derecha PLAYER_HEAD");
+
+                        Heads h = new Heads(plugin);
+
+                        List<String> head =  h.getHead(c_block);
+
+                        UUID uuid = UUID.fromString(head.get(0));
+
+                        String name = head.get(1);
+
+                        plugin.print("uuid: " + uuid);
+                        plugin.print("name: " + name);
+
+                    }
+                }
+
+            }
+        }
+
+        plugin.print("Revivir derecha");
+    }
+
+    private void leftOrient(Block block, List<Block> structure){
+        plugin.print("Revivir izquierda");
+    }
+
+    private void backOrient(Block block, List<Block> structure){
+        plugin.print("Revivir atras");
+    }
+
+    private void frontOrient(Block block, List<Block> structure){
+        plugin.print("Revivir adelante");
     }
 }
