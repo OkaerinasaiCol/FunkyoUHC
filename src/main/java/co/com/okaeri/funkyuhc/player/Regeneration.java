c4045abb422a24240e67e7f78cd5f3e07b0158fa
funkyo <enmafunkyo@gmail.com>
2022-11-11 13:27:30 -0500

    Se crea clase para el control de la regeneración

---
+++ src/main/java/co/com/okaeri/funkyuhc/player/Regeneration.java
@@ -0,0 +1,34 @@
+package co.com.okaeri.funkyuhc.player;
+
+import co.com.okaeri.funkyuhc.FunkyUHC;
+import org.bukkit.GameRule;
+import org.bukkit.World;
+
+public class Regeneration {
+
+    private FunkyUHC plugin;
+    public boolean isEnabled;
+
+    public Regeneration(FunkyUHC plugin){
+        this.plugin = plugin;
+        isEnabled = true; // TODO: configurar para obtener de si el uhc está en curso o no
+    }
+
+    public void enableRegeneration(){
+        for (World world: plugin.getServer().getWorlds()){
+            world.setGameRule(GameRule.NATURAL_REGENERATION, false);
+        }
+        isEnabled = true;
+    }
+
+    public void disableRegeneration(){
+        for (World world: plugin.getServer().getWorlds()){
+            world.setGameRule(GameRule.NATURAL_REGENERATION, true);
+        }
+        isEnabled = false;
+    }
+
+    public boolean getState(){
+        return isEnabled;
+    }
+}
