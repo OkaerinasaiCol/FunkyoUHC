22bac2a6393a97de80334c5844aec6441a6a5091
Funkyo <enmafunkyo@gmail.com>
2022-11-14 15:18:21 -0500

    Se crea función para cuando se escriba algo en el chat y ya este iniciado el uhc

---
+++ src/main/java/co/com/okaeri/funkyuhc/player/ChatListener.java
@@ -0,0 +1,56 @@
+package co.com.okaeri.funkyuhc.player;
+
+import co.com.okaeri.funkyuhc.FunkyUHC;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatListener implements Listener {
+
+    @SuppressWarnings("FieldMayBeFinal")
+    private FunkyUHC plugin;
+
+    public ChatListener(FunkyUHC plugin){
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void chat(AsyncPlayerChatEvent event) {
+
+        if (plugin.UhcStarted && (event.getPlayer().getGameMode() != GameMode.SPECTATOR)) {
+            Player p = event.getPlayer();
+            String message = event.getMessage();
+            event.setCancelled(true);
+
+            String team = plugin.TeamDB.getTeam(p.getName());
+            String capitan = plugin.TeamDB.getTeamCapitan(team);
+
+            try {
+
+                if(!(p.getName().equals(capitan))) {
+                    //noinspection ConstantConditions
+                    plugin.getServer().getPlayer(capitan).sendMessage("<" + p.getName() + "> " + message);
+                }
+
+            } catch (NullPointerException ignored){}
+
+            //noinspection ConstantConditions
+            plugin.print(p.getPlayer().getName() + " m: " + message);
+
+            for (String team_players: plugin.TeamDB.getTeamPlayers(team)){
+                Player team_player = plugin.getServer().getPlayer(team_players);
+
+                try {
+                    if (!(team_players.equals(p.getName()))) {
+                        //noinspection ConstantConditions
+                        team_player.sendMessage("<" + p.getName() + "> " + message);
+                    }
+                } catch (NullPointerException ignored){}
+
+            }
+
+        }
+    }
+}
