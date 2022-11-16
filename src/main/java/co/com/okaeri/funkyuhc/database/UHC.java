8a6451f108c6761107f3c50ece4796dd9f11290e
funkyo <enmafunkyo@gmail.com>
2022-11-16 15:47:59 -0500

    Se agrega clase para el manejo de la tabla referente al estado del uhc por si se cierra en ejecución

---
+++ src/main/java/co/com/okaeri/funkyuhc/database/UHC.java
@@ -0,0 +1,14 @@
+package co.com.okaeri.funkyuhc.database;
+
+import co.com.okaeri.funkyuhc.FunkyUHC;
+
+public class UHC {
+
+    private FunkyUHC plugin;
+
+    public UHC(FunkyUHC plugin){
+        this.plugin = plugin;
+    }
+
+    public void addInfo(){}
+}
